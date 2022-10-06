4e6e5d5d5df472e422f5e727e7267a5fa0b21b37
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-06 16:19:42 -0400

    Commit do exercício 04 utilizando If/Else
    Esta atividade faz parte do conjunto de 7 exercícios utilizando a lógica de If e Else, aprendida durante aula ministrada pelo professor Clayton Ferraz

---
+++ Atividade (05-09-22)/EXERCICIO_04/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 04:
+
+            // Faça um algoritmo que leia um número inteiro e mostre uma mensagem indicando
+            // se este número é par ou impar, e se é positivo ou negativo.
+
+            // Variáveis:
+
+            int num = 0;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite um número (posivo ou negativo):");
+            num = int.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados:
+            if (num == 0)
+            {
+                Console.WriteLine("O seu número é NEUTRO, não ente bugar o sitema, espertinho >:(");
+                Console.WriteLine("Conta finalizada!");
+            }
+            else if (num % 2 == 0 && num >= 0)
+            {
+                Console.WriteLine("O seu número é PAR e POSITIVO");
+                Console.WriteLine("Conta finalizada!");
+            }
+            else if (num % 2 == 0 && num < 0)
+            {
+                Console.WriteLine("O seu número é PAR e NEGATIVO");
+                Console.WriteLine("Conta finalizada!");
+            }
+            else if (num % 2 != 0 && num >= 0)
+            {
+                Console.WriteLine("O seu número é ÍMPAR e POSITIVO");
+                Console.WriteLine("Conta finalizada!");
+            }
+            else
+            {
+                Console.WriteLine("O seu número é ÍMPAR e NEGATIVO");
+                Console.WriteLine("Conta finalizada!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
