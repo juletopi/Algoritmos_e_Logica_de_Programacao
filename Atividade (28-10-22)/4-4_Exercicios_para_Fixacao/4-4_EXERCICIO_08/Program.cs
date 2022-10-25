be01f9f550f44e87297a3b448272df53f66262db
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-25 19:20:05 -0400

    Commit do exercício 08 do conjunto 4.4
    Esta atividade faz parte do conjunto 4.4 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/4-4_Exercicios_para_Fixacao/4-4_EXERCICIO_08/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_EXERCICIO_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 08:
+
+            // De acordo com o valor fornecido pelo usuário, verifique se ele é múltiplo de 3 ou múltiplo
+            // de 7. Apresente uma mensagem mostrando o número digitado e o resultado do teste.
+
+            // Variáveis:
+            double num = 0;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite um número:");
+            num = double.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados:
+            if (num == 0)
+            {
+                Console.WriteLine("O número escolhido foi: " + num);
+                Console.WriteLine("Este é um número NEUTRO!");
+            }
+            else
+            {
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("O número escolhido foi: " +num);
+                    Console.WriteLine("Este é um número é multiplo de 3!");
+                }
+                if (num % 7 == 0)
+                {
+                    Console.WriteLine("O número escolhido foi: " +num);
+                    Console.WriteLine("Este é um número é multiplo de 7!");
+                }
+                else
+                {
+                    if (num % 3 != 0 && num % 3 != 0)
+                    {
+                        Console.WriteLine("O número escolhido foi: " +num);
+                        Console.WriteLine("Este é um número é não é multiplo de 3 ou 7!");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
