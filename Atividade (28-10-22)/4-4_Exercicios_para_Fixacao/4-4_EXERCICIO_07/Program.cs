72f7191972914e454fa9ef3a90b2e5a7bdcab7b1
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-25 19:18:46 -0400

    Commit do exercício 07 do conjunto 4.4
    Esta atividade faz parte do conjunto 4.4 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/4-4_Exercicios_para_Fixacao/4-4_EXERCICIO_07/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_EXERCICIO_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 07:
+
+            // Melhore o algoritmo do exercício anterior (Exercício 06), verificando se o número
+            // inserido pelo usuário é zero/neutro, par ou ímpar.
+
+            // Variáveis:
+            double num = 0;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite um número:");
+            num = double.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados:
+            if (num == 0)
+            {
+                Console.WriteLine("O número escolhido foi: " + num);
+                Console.WriteLine("Este é um número ZERADO/NEUTRO!");
+            }
+            else
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("O número escolhido foi: " + num);
+                    Console.WriteLine("Este é um número PAR!");
+                }
+                else
+                {
+                    Console.WriteLine("O número escolhido foi: " + num);
+                    Console.WriteLine("Este é um número ÍMPAR!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
