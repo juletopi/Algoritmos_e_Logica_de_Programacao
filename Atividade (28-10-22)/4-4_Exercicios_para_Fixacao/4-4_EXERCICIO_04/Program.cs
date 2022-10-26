03510ba21fd4e0dca1b640088a8a93cd5592be56
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-26 16:04:59 -0400

    Commit do exercício 04 do conjunto 4.4
    Esta atividade faz parte do conjunto 4.4 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/4-4_Exercicios_para_Fixacao/4-4_EXERCICIO_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_EXERCICIO_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 04:
+
+            // Uma empresa concederá um reajuste de 8,75% no próximo mês. Elabore um algoritmo que, a partir
+            // dos dados inseridos pelo usuário, calcule o salário reajustado de um funcionário, exibindo
+            // como resultado, o seu nome, o valor do seu salário atual e o valor do salário reajustado.
+
+            // Variáveis:
+            double salario = 0, salario_total = 0;
+            double reajuste = 8.75, reajuste_valor = 0;
+            string nome;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite o nome do funcionário:");
+            nome = Console.ReadLine();
+            Console.WriteLine("\r\nDigite o salário do funcionário:");
+            salario = double.Parse(Console.ReadLine());
+
+            // Processamento:
+            reajuste_valor = (reajuste / 100) * salario;
+            salario_total = reajuste_valor + salario;
+
+            // Saída dos Dados:
+            Console.WriteLine("\r\nO salário do funcionário " +nome +", é de: R$" +salario +"\r\nO salário deste funcionário, pós reajuste de 8,75%, é de: R$" +salario_total);
+            Console.ReadKey();
+        }
+    }
+}
