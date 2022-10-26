5ca0ce0004380567415b21bc87152ef841e246f2
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-26 16:01:45 -0400

    Commit do exercício 02 do conjunto 4.4
    Esta atividade faz parte do conjunto 4.4 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/4-4_Exercicios_para_Fixacao/4-4_EXERCICIO_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_EXERCICIO_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 02:
+
+            // Leia 3 valores, calcule a média aritmética entre eles e apresente o resultado.
+
+            // Variáveis:
+            double valor1 = 0, valor2 = 0, valor3 = 0;
+            double total;
+            double media_a;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite 3 valores:");
+            valor1 = double.Parse(Console.ReadLine());
+            valor2 = double.Parse(Console.ReadLine());
+            valor3 = double.Parse(Console.ReadLine());
+
+            // Processamento:
+            total = valor1 + valor2 + valor3;
+            media_a = total / 3;
+
+            // Saída dos Dados:
+            Console.WriteLine("\r\nA média aritmética dos números é: " +media_a +"\r\nConta finalizada!");
+            Console.ReadKey();
+        }
+    }
+}
