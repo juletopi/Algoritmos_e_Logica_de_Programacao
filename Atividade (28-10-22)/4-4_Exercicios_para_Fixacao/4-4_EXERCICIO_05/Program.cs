0b70e0afbda3d72a56361ec6a93091f520b660bc
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-26 16:05:55 -0400

    Commit do exercício 05 do conjunto 4.4
    Esta atividade faz parte do conjunto 4.4 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/4-4_Exercicios_para_Fixacao/4-4_EXERCICIO_05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_EXERCICIO_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 05:
+
+            // As lojas de um shopping center estão concedendo 10% de desconto no preço de qualquer
+            // produto. Elabore um algoritmo que a partir do valor fornecido, calcule e exiba o
+            // preço atual e o preço com desconto.
+
+            // Variáveis:
+            double valor_produto = 0, valor_desconto = 0;
+            double desconto = 10;
+            string produto;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite o nome do produto:");
+            produto = Console.ReadLine();
+            Console.WriteLine("\r\nDigite o valor do produto:");
+            valor_produto = double.Parse(Console.ReadLine());
+
+            // Processamento:
+            valor_desconto = valor_produto - (valor_produto * (desconto / 100));
+
+            // Saída dos Dados:
+            Console.WriteLine("\r\nO valor do produto " +produto +", é de: R$" +valor_produto +"\r\nO valor deste produto, com desconto de 10%, é de: R$" +valor_desconto);
+            Console.ReadKey();
+        }
+    }
+}
