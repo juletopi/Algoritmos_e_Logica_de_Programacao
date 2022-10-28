2ef16ce144eedb9fd8ecf2be6605eb8cde921368
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-10-27 21:27:09 -0400

    Commit da atividade de Tabuada em CSharp
    Nesta atividade, usou-se conceitos simples de "For/While", ensinados em aula e aplicados com o Visual Studio em CSharp

---
+++ Tabuada_CSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estruturas de repetição: FOR e WHILE
+
+            // As estruturas de repetição são utilizadas para processar uma coleção
+            // de dados, linhas de um arquivo, registros de um banco de dados que
+            // precisam ser processados por um mesmo bloco de código.
+
+            // O FOR é mais usado para loops definidos e WHILE para loops infinitos.
+            // Ou seja, com o WHILE o programa só para de "loopar" quando
+            // uma condição ocorre, já o FOR possui um limite.
+
+            // Exemplo usando o FOR: Tabuada de um número até onde o usuário quiser
+
+            // Variáveis:
+            int i, n, m;
+            int resultado;
+
+            // Entrada de Dados:
+            Console.WriteLine("Defina um número para exibir a tabuada dele:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\r\nQuantas vezes?");
+            m = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            // Processamento e Saída de Dados:
+            for (i = 1; i <= m; i++)
+            {
+                resultado = n * i;
+                Console.WriteLine(n + " x " + i + " = " + resultado);
+            }
+            Console.ReadKey();
+        }
+    }
+}
