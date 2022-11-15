a9bf06c0afbad804bffef3c4cad79efec222be4f
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-11-14 22:56:58 -0400

    Commit do exercício 05 do conjunto 5.5
    Esta atividade faz parte do conjunto 5.5 de exercícios de fixação, valendo 40pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-10-22)/5-5_Exercicios_para_Fixacao/5-5_EXERCICIO_05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_5_EXERCICIO_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 05:
+
+            // Elabore um algoritmo que a partir do número (n) fornecido pelo usuário,
+            // execute os cálculos e seguir, enquanto este número for diferente de 1.
+
+            // a) Se n for par, n = n ÷ 2
+            // b) Se n for ímpar, n = n x 3 + 1
+            
+            // Variáveis:
+            double num = 0;
+            double calc_par, calc_impar;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite um número:");
+            num = double.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados:
+
+            if (num % 2 == 0)
+            {
+                calc_par = (num / 2);
+                Console.WriteLine("\r\nSeu número é PAR, e o seu cálculo de (n = n ÷ 2) é: " +calc_par +"\r\nConta finalizada!");
+            }
+            else
+            {
+                calc_impar = (num * 3 + 1);
+                Console.WriteLine("\r\nSeu número é ÍMPAR, e o seu cálculo de (n = n x 3 + 1) é: " +calc_impar +"\r\nConta finalizada!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
