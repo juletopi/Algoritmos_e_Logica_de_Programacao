02cf7df7611c12fa3bbe492b1c2eda22741c2167
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-12-08 16:22:29 -0400

    Commit do exercício extra 03
    Esta atividade faz parte do conjunto de exercícios extras, valendo 20pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (24-11-22)/EXERCICIO_03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 03
+
+            // Faça um algoritmo que leia um número inteiro e mostre uma
+            // mensagem indicando se este número é par ou ímpar, e se é
+            // positivo ou negativo.
+
+            // Variáveis:
+            double num = 0;
+
+            // Entrada de Dados:
+            Console.WriteLine("Digite um número: ");
+            num = double.Parse(Console.ReadLine());
+
+            // Processamento e Saída de Dados:
+
+            Console.Write("\r\n ┌─────────────┐");
+            Console.Write("\r\n │ Resultados: │");
+            Console.Write("\r\n └─────────────┘\r\n");
+
+            if (num == 0)
+            {
+                Console.WriteLine("\r\nSeu número é NULO!");
+            }
+            if (num % 2 == 0 && num != 0)
+            {
+                Console.WriteLine("\r\nSeu número é PAR!");
+            }
+            if (num % 2 != 0 && num != 0)
+            {
+                Console.WriteLine("\r\nSeu número é ÍMPAR!");
+            }
+            if (num > 0 && num != 0)
+            {
+                Console.WriteLine("Seu número é POSITIVO!");
+            }
+            if (num < 0 && num != 0)
+            {
+                Console.WriteLine("Seu número é NEGATIVO!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
