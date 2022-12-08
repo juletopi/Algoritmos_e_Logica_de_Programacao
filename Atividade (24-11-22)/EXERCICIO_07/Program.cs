f571a9c988abe48f087c3603d8a801f894f15d2a
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-12-08 16:29:48 -0400

    Commit do exercício extra 07
    Esta atividade faz parte do conjunto de exercícios extras, valendo 20pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (24-11-22)/EXERCICIO_07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 07
+
+            // Escreva um algoritmo que você fez durante o semestre. 
+
+            // "Tabuada_CSharp"
+
+            // Variáveis:
+            int i, n, m;
+            int resultado;
+
+            // Entrada de Dados:
+            Console.WriteLine("Defina um número para exibir a tabuada dele:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\r\nQuantas vezes?");
+            m = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            // Processamento e Saída de Dados:
+            for (i = 1; i <= m; i++)
+            {
+                resultado = n * i;
+                Console.WriteLine(n + " x " + i + " = " + resultado);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
