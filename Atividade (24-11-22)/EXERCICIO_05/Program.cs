8041bfd836656c4e785b64afd63862b1a8049f3f
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-12-08 16:25:36 -0400

    Commit do exercício extra 05
    Esta atividade faz parte do conjunto de exercícios extras, valendo 20pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (24-11-22)/EXERCICIO_05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 05
+
+            // Faça um algoritmo que imprima a tabuada de 1 a 10. 
+
+            // Variáveis:
+            int resultado;
+
+            // Entrada, Processamento e Saída de Dados:
+
+            Console.Write("\r\n ┌────────────────────────────────┐");
+            Console.Write("\r\n │ Mostrando a tabuada de 1 a 10: │");
+            Console.Write("\r\n └────────────────────────────────┘\r\n");
+            Console.WriteLine("");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    resultado = i * j;
+                    Console.WriteLine(i + " x " + j + " = " + resultado);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
