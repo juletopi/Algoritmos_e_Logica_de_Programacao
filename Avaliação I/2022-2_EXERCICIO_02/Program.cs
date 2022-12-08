94aaf910a5c7ab70b0dfde8619f146a7ca357e87
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-12-08 16:38:08 -0400

    Commit do exercício 02 da Avaliação I
    Esta atividade faz parte do conjunto da Avaliação I, valendo 60pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Avaliação I/2022-2_EXERCICIO_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_2_EXERCICIO_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 02
+
+            // Tire a média dos números de 1 a 100 e imprima o valor da média.
+
+            // Variáveis:
+            double media = 0;
+
+            // Entrada, Processamento e Saída de Dados:
+
+            for (int i = 0; i <= 100; i++)
+            {
+                media = i / 100;
+            }
+            Console.Write("\r\n ┌──────────────────────────────┐");
+            Console.Write("\r\n │ Imprimindo o valor da média: │");
+            Console.Write("\r\n └──────────────────────────────┘\r\n");
+            Console.WriteLine("\r\nA média dos números de 1 a 100, é: " + media);
+
+            Console.ReadKey();
+        }
+    }
+}
