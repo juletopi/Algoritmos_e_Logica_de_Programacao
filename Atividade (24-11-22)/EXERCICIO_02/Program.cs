f070a567f50419bddc4fc3110e84af833ae33f60
Júlio Cézar <juliocezarpvh@hotmail.com>
2022-12-08 16:21:40 -0400

    Commit do exercício extra 02
    Esta atividade faz parte do conjunto de exercícios extras, valendo 20pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (24-11-22)/EXERCICIO_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 02
+
+            // Some os números de 1 a 100 a imprima o valor.
+
+            // Variáveis:
+            int soma = 0;
+
+            // Entrada, Processamento e Saída de Dados:
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                soma = soma + i;
+            }
+
+            Console.Write("\r\n ┌──────────────────────────────────────────────┐");
+            Console.Write("\r\n │ Some os números de 1 a 100 a imprima o valor │");
+            Console.Write("\r\n └──────────────────────────────────────────────┘\r\n");
+            Console.WriteLine("\r\nA soma dos números de 1 a 100, é: " +soma);
+
+            Console.ReadKey();
+        }
+    }
+}
