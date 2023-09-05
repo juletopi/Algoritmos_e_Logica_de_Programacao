237b38f88ce5898d7257b7e4809635d1a7e7e592
Júlio Cézar <juliocezarpvh@hotmail.com>
2023-09-04 21:24:51 -0400

    Commit do conjunto de 2 exercícios de Matrizes
    Esta atividade faz parte do conjunto de exercícios de matrizes, valendo 5pts para a disciplina de Algoritmos e Lógica de Programação do professor Clayton Ferraz

---
+++ Atividade (28-09-23)/EXERCICIO_01/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Faça um cadastro de alunos, onde cada aluno tem diferentes
+            // disciplinas e cada disciplina tem uma nota diferentes.
+
+            // I) Cadastrando os alunos e a quant. de disciplinas
+            Console.Write("\r\n ┌─────────────────────────────────────────┐");
+            Console.Write("\r\n │ Cadastro de alunos, disciplinas e notas │");
+            Console.Write("\r\n └─────────────────────────────────────────┘\r\n");
+            Console.WriteLine(" ");
+
+            Console.WriteLine(" Quantos alunos você deseja cadastrar?");
+            int quantAlunos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Quantas disciplinas cada aluno tem?");
+            int quantDisciplinas = Convert.ToInt32(Console.ReadLine());
+
+            string[][] nomesAlunos = new string[quantAlunos][];
+            string[][] nomesDisciplinas = new string[quantAlunos][];
+            double[][] notas = new double[quantAlunos][];
+
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.Write("\r\n ┌────────────────────────────────────────────┐");
+            Console.Write("\r\n │ Informando os dados dos alunos cadastrados │");
+            Console.Write("\r\n └────────────────────────────────────────────┘\r\n");
+
+            for (int i = 0; i < quantAlunos; i++)
+            {
+                Console.Write(" \r\n Digite o nome do aluno nº" + (i + 1) + ": ");
+                string nomedoAluno = Console.ReadLine();
+                Console.WriteLine(" ");
+
+                // II) Iniciando as matrizes internas a partir do número de disciplinas
+                nomesAlunos[i] = new string[] { nomedoAluno };
+                nomesDisciplinas[i] = new string[quantDisciplinas];
+                notas[i] = new double[quantDisciplinas];
+
+                for (int j = 0; j < quantDisciplinas; j++)
+                {
+                    Console.Write(" Digite o nome da disciplina nº" + (j + 1) + " para o aluno " + nomedoAluno + ": ");
+                    nomesDisciplinas[i][j] = Console.ReadLine();
+
+                    Console.Write(" Digite a nota da disciplina nº" + (j + 1) + " para o aluno " + nomedoAluno + ": ");
+                    notas[i][j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            // III) Imprimindo os dados dos alunos e as suas notas
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.Write("\r\n ┌────────────┐");
+            Console.Write("\r\n │ Resultados │");
+            Console.Write("\r\n └────────────┘\r\n");
+
+            for (int i = 0; i < quantAlunos; i++)
+            {
+                Console.WriteLine(" \r\n Aluno: " + nomesAlunos[i][0]);
+                Console.WriteLine(" Disciplinas e Notas: ");
+                Console.WriteLine(" ");
+
+                for (int j = 0; j < quantDisciplinas; j++)
+                {
+                    Console.WriteLine(" " + nomesDisciplinas[i][j] + ": " + notas[i][j] + "pts");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("\r\n Pressione ENTER para finalizar.");
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Atividade (28-09-23)/EXERCICIO_02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. Faça um cadastro de fazendeiros, onde cada fazendeiro tem diferentes tipos de gado
+            // (bezerro, novilha, garote, vaca, boi) e cada tipo tem valor e quantidade diferente.
+
+            // I) Cadastrando os fazendeiros e a quant. de gados para cadastrar
+            Console.Write("\r\n ┌─────────────────────────────────────────────────┐");
+            Console.Write("\r\n │ Cadastro de fazendeiros, gados e quant. do gado │");
+            Console.Write("\r\n └─────────────────────────────────────────────────┘\r\n");
+            Console.WriteLine(" ");
+
+            Console.WriteLine(" Quantos fazendeiros você deseja cadastrar?");
+            int quantFazendeiros = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Quantos gados cada fazendeiro tem?");
+            int quantGados = Convert.ToInt32(Console.ReadLine());
+
+            string[][] nomesFazendeiros = new string[quantFazendeiros][];
+            string[][] nomesGados = new string[quantFazendeiros][];
+            double[][] quantDoGado = new double[quantFazendeiros][];
+
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.Write("\r\n ┌─────────────────────────────────────────────────┐");
+            Console.Write("\r\n │ Informando os dados dos fazendeiros cadastrados │");
+            Console.Write("\r\n └─────────────────────────────────────────────────┘\r\n");
+
+            for (int i = 0; i < quantFazendeiros; i++)
+            {
+                Console.Write(" \r\n Digite o nome do fazendeiro nº" + (i + 1) + ": ");
+                string nomedoFazendeiro = Console.ReadLine();
+                Console.WriteLine(" ");
+
+                // II) Iniciando as matrizes internas a partir do número de gados
+                nomesFazendeiros[i] = new string[] { nomedoFazendeiro };
+                nomesGados[i] = new string[quantGados];
+                quantDoGado[i] = new double[quantGados];
+
+                for (int j = 0; j < quantGados; j++)
+                {
+                    Console.Write(" Digite o nome do gado nº" + (j + 1) + " para o fazendeiro " + nomedoFazendeiro + ": ");
+                    nomesGados[i][j] = Console.ReadLine();
+
+                    Console.Write(" Digite a quantidade de cabeças do gado nº" + (j + 1) + " para o fazendeiro " + nomedoFazendeiro + ": ");
+                    quantDoGado[i][j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            // III) Imprimindo os dados dos alunos e as suas notas
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.Write("\r\n ┌────────────┐");
+            Console.Write("\r\n │ Resultados │");
+            Console.Write("\r\n └────────────┘\r\n");
+
+            for (int i = 0; i < quantFazendeiros; i++)
+            {
+                Console.WriteLine(" \r\n Fazendeiro: " + nomesFazendeiros[i][0]);
+                Console.WriteLine(" Gados e Cabeças: ");
+                Console.WriteLine(" ");
+
+                for (int j = 0; j < quantGados; j++)
+                {
+                    Console.WriteLine(" " + nomesGados[i][j] + ": " + quantDoGado[i][j] + " Cabeças");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("\r\n Pressione ENTER para finalizar.");
+            Console.WriteLine("\r\n -----------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
